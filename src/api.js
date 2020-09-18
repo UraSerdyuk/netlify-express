fe6407d2505a4aef49b1a8e825240fc25306579d
UraSerdyuk <yuraserdiuk@gmail.com>
2020-09-18 22:27:19 +0200

    added post routes

--- src/api.js
+++ src/api.js
@@ -8,20 +8,35 @@ const app = express();
 app.use(cors());
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const testUser = new test({ name: "testmessage" });
-  await testUser.save();
-
-  const arr = await test.find({});
-
+// /api/auth/register           registration
+router.post("/auth/register", async (req, res) => {
   res.json({
-    asljasiojf: "asdsadsad",
-    asljasiojf1: "asdsadsad",
-    asljasiojf2: "asdsadsad",
-    asljasioj1f: "asdsadsad",
-    asljasi2ojf: "asdsadsad",
-    asl2jasiojf: "asdsadsad",
-    arr: arr,
+    asljasiojf: "/register",
+  });
+});
+
+// /api/auth/login           registration
+router.post("/auth/login", async (req, res) => {
+  res.json({
+    asljasiojf: "/login",
+  });
+});
+
+router.get("/", async (req, res) => {
+  // const testUser = new test({ name: "testmessage" });
+  // await testUser.save();
+  // const arr = await test.find({});
+  // res.json({
+  //   asljasiojf: "asdsadsad",
+  //   asljasiojf1: "asdsadsad",
+  //   asljasiojf2: "asdsadsad",
+  //   asljasioj1f: "asdsadsad",
+  //   asljasi2ojf: "asdsadsad",
+  //   asl2jasiojf: "asdsadsad",
+  //   arr: arr,
+  // });
+  res.json({
+    asljasiojf: "/",
   });
 });
 
@@ -29,7 +44,7 @@ mongoose.connect(
   "mongodb+srv://cb8593bc:Racing-Bike-2000@cluster0.kmhay.gcp.mongodb.net/test1?retryWrites=true&w=majority"
 );
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
