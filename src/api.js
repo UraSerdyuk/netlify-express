cbe988546b8f65553bed8c8bbe956cafd574bed3
UraSerdyuk <yuraserdiuk@gmail.com>
2020-09-19 12:21:05 +0200

    test

--- src/api.js
+++ src/api.js
@@ -12,61 +12,61 @@ const app = express();
 app.use(cors());
 const router = express.Router();
 
-// /api/auth/register           registration
-router.post(
-  "/auth/register",
-  [
-    check("email", "Некорректный email").isEmail(),
-    check("password", "Минимальная длина пароля 6 символов").isLength({
-      min: 6,
-    }),
-  ],
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
+// // /api/auth/register           registration
+// router.post(
+//   "/auth/register",
+//   [
+//     check("email", "Некорректный email").isEmail(),
+//     check("password", "Минимальная длина пароля 6 символов").isLength({
+//       min: 6,
+//     }),
+//   ],
+//   async (req, res) => {
+//     try {
+//       const errors = validationResult(req);
 
-      if (!errors.isEmpty()) {
-        return res.status(400).json({
-          errors: errors.array(),
-          message: "Некорректный данные при регистрации",
-        });
-      }
+//       if (!errors.isEmpty()) {
+//         return res.status(400).json({
+//           errors: errors.array(),
+//           message: "Некорректный данные при регистрации",
+//         });
+//       }
 
-      const { email, password, firstName, lastName } = req.body;
+//       const { email, password, firstName, lastName } = req.body;
 
-      const candidate = await User.findOne({ email });
+//       const candidate = await User.findOne({ email });
 
-      if (candidate) {
-        return res
-          .status(400)
-          .json({ message: "Такой пользователь уже существует" });
-      }
+//       if (candidate) {
+//         return res
+//           .status(400)
+//           .json({ message: "Такой пользователь уже существует" });
+//       }
 
-      const hashedPassword = await bcrypt.hash(password, 12);
-      const user = new User({
-        email,
-        password: hashedPassword,
-        firstName,
-        lastName,
-      });
+//       const hashedPassword = await bcrypt.hash(password, 12);
+//       const user = new User({
+//         email,
+//         password: hashedPassword,
+//         firstName,
+//         lastName,
+//       });
 
-      await user.save();
+//       await user.save();
 
-      res.status(201).json({ message: "Пользователь создан" });
-    } catch (e) {
-      res
-        .status(500)
-        .json({ message: "Что-то пошло не так, попробуйте снова" });
-    }
-  }
-);
+//       res.status(201).json({ message: "Пользователь создан" });
+//     } catch (e) {
+//       res
+//         .status(500)
+//         .json({ message: "Что-то пошло не так, попробуйте снова" });
+//     }
+//   }
+// );
 
-// /api/auth/login           registration
-router.post("/auth/login", async (req, res) => {
-  res.json({
-    asljasiojf: "/login",
-  });
-});
+// // /api/auth/login           registration
+// router.post("/auth/login", async (req, res) => {
+//   res.json({
+//     asljasiojf: "/login",
+//   });
+// });
 
 router.get("/", async (req, res) => {
   // const testUser = new test({ name: "testmessage" });
@@ -86,9 +86,9 @@ router.get("/", async (req, res) => {
   });
 });
 
-mongoose.connect(
-  "mongodb+srv://cb8593bc:Racing-Bike-2000@cluster0.kmhay.gcp.mongodb.net/test1?retryWrites=true&w=majority"
-);
+// mongoose.connect(
+//   "mongodb+srv://cb8593bc:Racing-Bike-2000@cluster0.kmhay.gcp.mongodb.net/test1?retryWrites=true&w=majority"
+// );
 
 app.use(`/.netlify/functions/api`, router);
 
