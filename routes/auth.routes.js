f6cf9d3e5668f517f25f3db2f36949161fa55963
UraSerdyuk <yuraserdiuk@gmail.com>
2020-09-25 17:35:26 +0200

    8

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,65 +6,60 @@ const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = Router();
 
-// // test
-// router.post("/test", (req, res) => {
-//   res.json({ data: req.body });
-// });
-
 // // /api/auth/register
-// router.post(
-//   "/register",
-//   [
-//     check("email", "Некорректный email").isEmail(),
-//     check("password", "Минимальная длина пароля 6 символов").isLength({
-//       min: 6,
-//     }),
-//     check("firstName", "Неможе бути порожнім").isLength({
-//       min: 1,
-//     }),
-//     check("lastName", "Неможе бути порожнім").isLength({
-//       min: 1,
-//     }),
-//   ],
-//   async (req, res) => {
-//     try {
-//       const errors = validationResult(req);
+router.post(
+  "/register",
+  [
+    check("email", "Некорректный email").isEmail(),
+    check("password", "Минимальная длина пароля 6 символов").isLength({
+      min: 6,
+    }),
+    check("firstName", "Неможе бути порожнім").isLength({
+      min: 1,
+    }),
+    check("lastName", "Неможе бути порожнім").isLength({
+      min: 1,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-//       if (!errors.isEmpty()) {
-//         return res.status(400).json({
-//           errors: errors.array(),
-//           message: "Некорректный данные при регистрации",
-//         });
-//       }
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Некорректный данные при регистрации",
+        });
+      }
 
-//       const { email, password, firstName, lastName } = req.body;
+      const { email, password, firstName, lastName } = req.body;
 
-//       const candidate = await User.findOne({ email });
+      const candidate = await User.findOne({ email });
 
-//       if (candidate) {
-//         return res
-//           .status(400)
-//           .json({ message: "Такой пользователь уже существует" });
-//       }
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "Такой пользователь уже существует" });
+      }
 
-//       const hashedPassword = await bcrypt.hash(password, 12);
-//       const user = new User({
-//         firstName: firstName,
-//         lastName: lastName,
-//         email: email,
-//         password: hashedPassword,
-//       });
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: hashedPassword,
+      });
 
-//       await user.save();
+      await user.save();
 
-//       res.status(201).json({ message: "Пользователь создан" });
-//     } catch (e) {
-//       res
-//         .status(500)
-//         .json({ message: "Что-то пошло не так, попробуйте снова" });
-//     }
-//   }
-// );
+      res.status(201).json({ message: "Пользователь создан" });
+    } catch (e) {
+      res
+        .status(500)
+        .json({ message: "Что-то пошло не так, попробуйте снова" });
+    }
+  }
+);
 
 // /api/auth/login
 router.post(
